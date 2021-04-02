2073fd0e883a47af5e7f0eaf81ba0b659ed7e633
owalid <othmane.el.ayadi@gmail.com>
2021-04-02 12:05:48 +0200

    add last exercice dom

--- 07_DOM/04/script.js
+++ 07_DOM/04/script.js
@@ -1,3 +1,25 @@
+let can_open_alert = true
 const btn = document.getElementById('btn-popup')
 
-btn.addEventListener(() => alert("Hello je suis une alerte"))
+btn.addEventListener('click', () => {
+  console.log("can_open_alert", can_open_alert)
+  if (can_open_alert) {
+    alert("Hello je suis une alerte")
+  }
+})
+
+
+const p_elements = document.querySelectorAll('p')
+
+p_elements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    element.style.fontWeight = 'bold'
+  })
+})
+
+document.addEventListener("keyup", (event) => {
+  console.log("can_open_alert", can_open_alert)
+  if (event.key === 'Escape') {
+    can_open_alert = !can_open_alert
+  }
+})
